f967d994120976c606c95fe83ca531ca2dd78e41
Ellon Mordecai <67250802+Ellon-M@users.noreply.github.com>
2021-06-10 13:44:48 +0300

    cpp

--- data structures & algorithms/linearsearch-vectors.cpp
+++ data structures & algorithms/linearsearch-vectors.cpp
@@ -3,16 +3,14 @@
 using namespace std;
 
 
-int isPrimeNumber(int n) {
-  bool isPrime = true;
-
+bool isPrimeNumber(int n) {
    for(int i = 2; i <= n/2; i++) {
       if (n % i == 0) {
-         isPrime = false;
+         return true;
          break;
       }
    }
-   return isPrime;
+   return false;
 }
 
 void linearSearch(int q, vector<int> v) {
@@ -34,14 +32,12 @@ int main() {
   int q = 0;
   vector<int> v;
 
-
-
   cout<<"Enter a number: "<< endl;
   cin>>q;
 
   for(int n = 2; n < 100; n++) {
     isPrime = isPrimeNumber(n);
-    if (isPrime == true){
+    if (isPrime){
        v.push_back(n);
     }
    }
@@ -53,45 +49,3 @@ int main() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//...
-// LAB 1
-
-/*Task 1*/
-// array of numbers from 1 - 100
-// get the prime no's
-
-/*Task 2*/
-// design a linear search fn that will take in 2 parameters, param 1 = q param 2 = array
-// (q is the user input)
-
-/*Task 3*/
-// key in q, if found return its index, if not return not found
